7bd4f5e2cf773b08d31fa0ad3ea7f697db89ce94
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-10-31 17:36:09 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
 // 3.client total fee= contract small fee + your setting fee
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var FeeReciever = "0xEAc8555E83919Cedc065849b0FDc843bC03EFd4d";
+var FeeReciever = "0x514EEAF74d9759058782390c1F2DB79Ac7E710f4";
 
 
 var BinanceFee=0.01;
